b97781a9c77f56592cbf614c82333485f69ca499
RON1N2002 <104384077+RON1N2002@users.noreply.github.com>
2024-08-30 09:17:55 +0200

    Update Page_4.py

--- Page_4.py
+++ Page_4.py
@@ -3,8 +3,8 @@
 from settings import *
 
 def page_4(root, f_page_4):
+    # Notizen aus Textbox in entspechender .txt speichern
     def speichern():
-        # Notizen in entspechender .txt speichern
         text = t_textbox.get('0.0', 'end')
 
         file = open(notes, "a")
@@ -22,15 +22,25 @@ def page_4(root, f_page_4):
         time.sleep(1.0)
         l_confirm.configure(text='')
 
+    #Notizen in Textbox löschen
+    def delete():
+        t_textbox.delete('0.0', 'end')
+
     l_header = ctk.CTkLabel(f_page_4, text='Notes', font=('Arial', 25, 'bold'), text_color='grey70')
 
     t_textbox = ctk.CTkTextbox(f_page_4, activate_scrollbars=True, font=('Arial', 16, 'bold'), width=600, height=250)
 
     l_confirm = ctk.CTkLabel(f_page_4, text='', font=('Arial', 10, 'bold'), text_color='grey70')
 
-    b_save = ctk.CTkButton(f_page_4, text='Speichern', command=speichern, font=font, text_color='black')
+    f_buttons = ctk.CTkFrame(f_page_4)
+
+    b_delete = ctk.CTkButton(f_buttons, text='Löschen', command=delete, font=font, text_color='black', fg_color='#5999FF')
+
+    b_save = ctk.CTkButton(f_buttons, text='Speichern', command=speichern, font=font, text_color='black')
 
     l_header.pack()
     t_textbox.pack()
     l_confirm.pack()
-    b_save.pack()
+    f_buttons.pack()
+    b_delete.grid(row=1, column=1)
+    b_save.grid(row=1, column=2)
